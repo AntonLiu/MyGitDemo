b768cfb62debea26db151a23a58186b0d2308451
Anton.Liu <liuchundong2008@gmail.com>
2013-12-16 23:26:03 +0800

    first commit
    Signed-off-by: Anton.Liu <liuchundong2008@gmail.com>

---
+++ src/org/arcie/adong/gitdemo/MyFirstGitDemo.java
@@ -0,0 +1,14 @@
+package org.arcie.adong.gitdemo;
+/**
+ * @author chundong.liu
+ * @date 2013年12月16日下午10:40:07
+ * Description just build a demo using eclipse to push github
+ */
+/**
+ * @author chundong.liu
+ */
+public class MyFirstGitDemo {
+	public static void main(String[] args) {
+		System.out.println("MyFirstGitDemo Form My Home PC");
+	}
+}
