67e84c8e70b4a893dc95c457c293f2eb235b2f29
chundong.liu <liuchundong2008@gmial.com>
2013-12-17 13:29:47 +0800

    second commit
    Signed-off-by: chundong.liu <liuchundong2008@gmial.com>

--- src/org/arcie/adong/gitdemo/MyFirstGitDemo.java
+++ src/org/arcie/adong/gitdemo/MyFirstGitDemo.java
@@ -9,6 +9,8 @@ package org.arcie.adong.gitdemo;
  */
 public class MyFirstGitDemo {
 	public static void main(String[] args) {
-		System.out.println("MyFirstGitDemo Form My Home PC");
+		System.out.println("MyFirstGitDemo Form My Home PC");//2013年12月16日下午10:40:07
+		System.out.println("MyFirstGitDemo Form My Github");//2013年12月17日下午1:29:08
+		System.out.println("MySecondGitDemo Form My Office PC");//2013年12月24日下午1:08:25
 	}
 }
